79f87d0fb3e9dd3421a01a193f28e5dc2edff314
SwetaImmi <sweta.k@whizkraft.net>
2024-04-11 12:57:10 +0530

    Test

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="left-content">
                     <div class="thumb">
                         <div class="inner-content">
-                            <h4>We Are Hexashop</h4>
+                            <h4>We Are Test</h4>
                             <span>Awesome, clean &amp; creative HTML5 Template</span>
                             <div class="main-border-button">
                                 <a href="#">Purchase Now!</a>
